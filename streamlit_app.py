951f217cb41993961ec9c3e131201f5aac294a7e
Ely <107306325+Its-Ely@users.noreply.github.com>
2026-08-20 11:01:28 +0800

    Modify API call to use search_on for fruit lookup
    Updated API request to use search_on value for fruit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,11 +69,12 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+      
         search_on=pd_df.loc[pd_df['fruit_name'] == fruit_chosen, 'search_on'].iloc[0]
-        st.write('The search value for: ', fruit_chosen, ' is ', search_on,'.')
+        #st.write('The search value for: ', fruit_chosen, ' is ', search_on,'.')
       
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on}")  
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     st.write(ingredients_string, name_on_order)
